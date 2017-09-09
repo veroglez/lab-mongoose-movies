8e29e23fa85e409ce707fb59a8f3e829473b432a
VeroGlez <veronicagonzalezperez@MacBook-Pro-de-Veronica.local>
2017-09-09 19:49:39 +0200

    Refactoring code

--- starter-code/mongoose-movies/app.js
+++ starter-code/mongoose-movies/app.js
@@ -1,61 +1,19 @@
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+const app = require('express')();
 const mongoose = require('mongoose');
-const expressLayouts = require('express-ejs-layouts');
-const {dbURL} = require('./config/db');
+const globals = require('./config/db');
+
+mongoose.connect(globals.dbURL).then(() => console.log('Connected to DB'));
+
+app.locals.title = 'Mongoose movies'
+
+require('./config/express')(app)
+
 const layout = require('./routes/layout');
 const celebrities = require('./routes/celebrities');
-
-const app = express();
-
-mongoose.connect(dbURL, {useMongoClient: true})
-        .then(() => console.log('Conectado al a BBDD'));
-
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.set('layout','layout');
-app.use(expressLayouts);
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use((req,res,next) =>{
-  res.locals.title = 'Mongoose movies';
-  next();
-});
-
-app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.use('/', layout);
 app.use('/celebrities', celebrities);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+require('./config/handle-errors')(app)
 
 module.exports = app;
